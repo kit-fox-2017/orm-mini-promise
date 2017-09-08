f52d4d054aa71a5b55215a9c78d3c2bd6bb592bb
jainal arifin <jainalarifin@jainals-MacBook-Pro.local>
2017-09-08 10:59:09 +0700

    mencoba memindahkan ke orm

--- mini-orm/app.js
+++ mini-orm/app.js
@@ -1,14 +1,20 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 //---- percobaan
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./db/database.db');
 
 app.set('view engine', 'ejs')
-//
-// let project = require('./routes/project.js')
-//
-// app.use('/project', project)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+let project = require('./routes/project.js')
+app.use('/project', project)
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -21,8 +27,32 @@ app.get('/project', (req, res)=>{
   })
 })
 
-app.get('/project', (req, res)=>{
-  db.run(`INSERT INTO project(name, status) VALUES()`)
+app.post('/project', (req, res)=>{
+  db.run(`INSERT INTO project(name, status) VALUES('${req.body.name}', '${req.body.status}')`, ()=>{
+    res.redirect('/project')
+  })
 })
+
+app.get('/project/delete/:id', (req, res)=>{
+  db.run(`DELETE FROM project WHERE id=${req.params.id}`,()=>{
+    res.redirect('/project')
+  })
+})
+
+app.get('/project/edit/:id', (req, res)=>{
+  db.all(`SELECT * FROM project WHERE id=${req.params.id} `, (err, dataProject)=>{
+    // res.send(dataProject)
+    res.render('editProject', {dtProject:dataProject[0]})
+  })
+})
+
+app.post('/project/edit/:id', (req, res)=>{
+  db.run(`UPDATE project SET name='${req.body.name}', status='${req.body.status}' WHERE id=${req.params.id} `, ()=>{
+    res.redirect('/project')
+  })
+})
+
+
+
 
 app.listen(3000)
