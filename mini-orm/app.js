eda40d4337abc1360bb2997d070d2210643c18d8
jainal arifin <jainalarifin@jainals-MacBook-Pro.local>
2017-09-08 11:40:28 +0700

    ORM baru terbuat Project

--- mini-orm/app.js
+++ mini-orm/app.js
@@ -2,8 +2,6 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 //---- percobaan
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./db/database.db');
 
 app.set('view engine', 'ejs')
 
@@ -11,47 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
+let index = require('./routes/index.js')
 let project = require('./routes/project.js')
+
+app.use('/', index)
 app.use('/project', project)
-
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.get('/project', (req, res)=>{
-  db.all(`SELECT * FROM project`, (err, dataProject)=>{
-    // res.send(dataProject)
-    res.render('project', {dtProject:dataProject})
-  })
-})
-
-app.post('/project', (req, res)=>{
-  db.run(`INSERT INTO project(name, status) VALUES('${req.body.name}', '${req.body.status}')`, ()=>{
-    res.redirect('/project')
-  })
-})
-
-app.get('/project/delete/:id', (req, res)=>{
-  db.run(`DELETE FROM project WHERE id=${req.params.id}`,()=>{
-    res.redirect('/project')
-  })
-})
-
-app.get('/project/edit/:id', (req, res)=>{
-  db.all(`SELECT * FROM project WHERE id=${req.params.id} `, (err, dataProject)=>{
-    // res.send(dataProject)
-    res.render('editProject', {dtProject:dataProject[0]})
-  })
-})
-
-app.post('/project/edit/:id', (req, res)=>{
-  db.run(`UPDATE project SET name='${req.body.name}', status='${req.body.status}' WHERE id=${req.params.id} `, ()=>{
-    res.redirect('/project')
-  })
-})
-
 
 
 
--- mini-orm/models/project.js
+++ mini-orm/models/project.js
@@ -3,8 +3,9 @@ var db = new sqlite3.Database('./db/database.db');
 
 class ModelProject {
   constructor(raw) {
-    this.attribute1 = raw.attribute1
-    this.attribute2 = raw.attribute2
+    this.id = raw.id
+    this.name = raw.name
+    this.status = raw.status
   }
 
   static findAll(cb) {
@@ -12,19 +13,38 @@ class ModelProject {
       // let results = dataProject.map(m => new ModelProfile(m))
       // console.log(results);
       // cb(results)
-      cb(dataProject)
+      cb(err, dataProject)
     })
   }
 
-  static findById() {}
+  static findById(reqId, cb) {
+    db.all(`SELECT * FROM project WHERE id=${reqId} `, (err, dataProject)=>{
+    // res.send(dataProject)
+      cb(err, dataProject)
+    })
+  }
 
   static findWhere() {}
 
-  static create() {}
+  static create(reqBody, cb) {
+    db.run(`INSERT INTO project(name, status) VALUES('${reqBody.name}', '${reqBody.status}')`, ()=>{
+      // res.redirect('/project')
+      cb()
+    })
+  }
 
-  static update() {}
+  static update(reqId, reqBody, cb) {
+    db.run(`UPDATE project SET name='${reqBody.name}', status='${reqBody.status}' WHERE id=${reqId} `, ()=>{
+      // res.redirect('/project')
+      cb()
+    })
+  }
 
-  static destroy() {}
+  static destroy(reqId, cb) {
+    db.run(`DELETE FROM project WHERE id=${reqId}`,()=>{
+      cb()
+    })
+  }
 
 }
 
